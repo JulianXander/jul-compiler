be2a728f59f6589fb68592bf3fa453ffe58ebacb
JulianXander <JulianXander@ymail.com>
2023-03-06 15:58:56 +0100

    Cleanup

--- src/compiler.test.ts
+++ src/compiler.test.ts
@@ -145,7 +145,7 @@ const expectedResults: {
 		// 	code: 'fibonacci = (number:NonNegativeInteger) =>\n\tnumber ?\n\t\t(n:0) => 0\n\t\t(n:1) => 1\n\t\t(n) => sum(fibonacci(subtract(n 2)) fibonacci(subtract(n 1)))\nfibonacci(12)',
 		// 	result: 144
 		// },
-	];;
+	];
 
 // describe('Compiler', () => {
 // 	expectedResults.forEach(({ code, result }) => {
