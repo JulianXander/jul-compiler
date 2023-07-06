6fae5827834d8b314e0ad2b0527cee2a7da44269
JulianXander <julian.xander@ymail.com>
2023-07-06 22:29:41 +0200

    typescript parser wip

--- src/parser/typescript-parser.ts
+++ src/parser/typescript-parser.ts
@@ -1,6 +1,7 @@
-import typescript, { Node } from 'typescript';
-import { ParseExpression, ParsedFile } from '../syntax-tree.js';
+import { Name, ParseExpression, ParsedFile } from '../syntax-tree.js';
 import { isDefined } from '../util.js';
+import { Positioned } from './parser-combinator.js';
+import typescript, { BindingName, Identifier, Node, StringLiteral, VariableStatement } from 'typescript';
 const { createSourceFile, ScriptTarget, SyntaxKind } = typescript;
 
 
@@ -19,18 +20,73 @@ export function parseTsCode(code: string): ParsedFile {
 }
 
 function tsNodeToJulAst(tsNode: Node): ParseExpression | undefined {
+  const position = getPositionFromTsNode(tsNode);
   switch (tsNode.kind) {
+    case SyntaxKind.StringLiteral: {
+      const stringLiteral = tsNode as StringLiteral;
+      return {
+        type: 'string',
+        values: [{
+          type: 'stringToken',
+          value: stringLiteral.text,
+        }],
+        ...position,
+      };
+    }
     case SyntaxKind.NullKeyword:
       return {
         type: 'empty',
-        startRowIndex: tsNode.pos,
-        startColumnIndex: tsNode.pos,
-        endRowIndex: tsNode.end,
-        endColumnIndex: tsNode.end,
+        ...position,
       };
-
+    case SyntaxKind.VariableStatement: {
+      const variableStatement = tsNode as VariableStatement;
+      const test = variableStatement.declarationList.declarations.map(declaration => {
+        return {
+          name: tsNameToJulName(declaration.name),
+          value: declaration.initializer && tsNodeToJulAst(declaration.initializer),
+        };
+      });
+      const test1 = test[0]!;
+      return {
+        type: 'definition',
+        name: test1.name,
+        value: test1.value as any,
+        ...position,
+      };
+    }
     case SyntaxKind.TypeAliasDeclaration:
     default:
       return undefined;
+  }
+}
+
+function tsNameToJulName(tsName: BindingName): Name {
+  // TODO case BindingPattern
+  let name: string;
+  switch (tsName.kind) {
+    case SyntaxKind.Identifier:
+      name = tsName.text;
+      break;
+    case SyntaxKind.ObjectBindingPattern:
+    case SyntaxKind.ArrayBindingPattern:
+      throw new Error(`SyntaxKind for Name not implemented yet: ${SyntaxKind[tsName.kind]}`);
+    default:
+      const assertNever: never = tsName;
+      throw new Error(`Unexpected SyntaxKind for Name: ${SyntaxKind[(assertNever as BindingName).kind]}`);
+  }
+  return {
+    type: 'name',
+    name: tsName.text,
+    ...getPositionFromTsNode(tsName),
+  }
+}
+
+function getPositionFromTsNode(tsNode: Node): Positioned {
+  // TODO
+  return {
+    startRowIndex: tsNode.pos,
+    startColumnIndex: tsNode.pos,
+    endRowIndex: tsNode.end,
+    endColumnIndex: tsNode.end,
   }
 }
