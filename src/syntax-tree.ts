1fdbbbb2706175d54d6624f89436ffb931d70939
JulianXander <julian.xander@ymail.com>
2026-09-10 09:36:35 +0200

    kommentar

--- src/syntax-tree.ts
+++ src/syntax-tree.ts
@@ -722,7 +722,8 @@ export function createCompileTimeComplementType(SourceType: CompileTimeType): Co
 }
 
 /**
- * Untertyp aller Werte, die größer als Value sind (z.B. Greater(0) für PositiveInteger).
+ * Menge aller Werte, die größer als Value sind (nur in Kombination mit einem anderen Typ
+ * sinnvoll, z.B. And(Integer Greater(0)) für PositiveInteger).
  */
 export interface CompileTimeGreaterType extends CompileTimeTypeBase {
 	readonly julType: 'greater';
