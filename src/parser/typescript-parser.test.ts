6231abb8ddf8d81a44975fd6d7e42e18f3a92ed4
JulianXander <julian.xander@ymail.com>
2026-09-15 10:11:40 +0200

    roter test typescript parser

---
+++ src/parser/typescript-parser.test.ts
@@ -0,0 +1,17 @@
+import { expect } from 'chai';
+import { parseTsCode } from './typescript-parser.js';
+import { ParseSingleDefinition } from '../syntax-tree.js';
+
+describe('TypeScript Parser', () => {
+	it('sollte Zeile/Spalte statt rohem Zeichen-Offset für die Position einer Funktionsdeklaration liefern', () => {
+		const code = 'export function foo() {\n\treturn 1;\n}\n';
+		const result = parseTsCode(code);
+		const definition = result.expressions![0] as ParseSingleDefinition;
+		expect(definition.name).to.deep.include({
+			startRowIndex: 0,
+			startColumnIndex: 17,
+			endRowIndex: 0,
+			endColumnIndex: 20,
+		});
+	});
+});
